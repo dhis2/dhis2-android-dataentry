f07dbf2b9c566bf9a97a23328abdd8f201ae5abc
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-01-16 12:36:23 +0100

    Fabric answers configuration

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/DhisApp.java
@@ -0,0 +1,68 @@
+package org.hisp.dhis.android.dataentry;
+
+import android.app.Application;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import hu.supercluster.paperwork.Paperwork;
+import io.fabric.sdk.android.Fabric;
+
+public class DhisApp extends Application {
+    private static final String GIT_SHA = "gitSha";
+    private static final String BUILD_DATE = "buildDate";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // fail early on leaks
+        setStrictMode();
+
+        Paperwork paperwork = setUpPaperwork();
+        setUpFabric(paperwork);
+    }
+
+    @NonNull
+    private Paperwork setUpPaperwork() {
+        return new Paperwork(this);
+    }
+
+    private void setUpFabric(@NonNull Paperwork paperwork) {
+        if (BuildConfig.DEBUG) {
+            // Set up Crashlytics, disabled for debug builds
+            Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                    .core(new CrashlyticsCore.Builder()
+                            .disabled(true)
+                            .build())
+                    .build();
+
+            // Initialize Fabric with the debug-disabled crashlytics.
+            // Fabric.with(this, crashlyticsKit);
+        } else {
+            Crashlytics crashlytics = new Crashlytics();
+            crashlytics.core.setString(GIT_SHA, paperwork.get(GIT_SHA));
+            crashlytics.core.setString(BUILD_DATE, paperwork.get(BUILD_DATE));
+
+            Fabric.with(this, new Crashlytics(), new Answers());
+        }
+    }
+
+    private void setStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+    }
+}
---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/LauncherActivity.java
@@ -0,0 +1,13 @@
+package org.hisp.dhis.android.dataentry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
