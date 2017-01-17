b5b0efc06fa6752d7e0e659af99f7c0b650a2393
luckytommy <thomas.lindsjorn@gmail.com>
2017-01-17 14:41:43 +0100

    Moved LauncherActivity and App launcher icon to ui module

--- ui/src/main/java/org/hisp/dhis/android/ui/LauncherActivity.java
+++ ui/src/main/java/org/hisp/dhis/android/ui/LauncherActivity.java
@@ -26,7 +26,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.hisp.dhis.android.dataentry;
+package org.hisp.dhis.android.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
