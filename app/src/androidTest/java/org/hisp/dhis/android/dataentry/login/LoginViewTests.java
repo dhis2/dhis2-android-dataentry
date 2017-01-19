8040b491ba3983df1e8126eb954b0f87c87897a4
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-01-19 17:27:26 +0100

    Refactored login view espresso tests to follow robot pattern.

--- app/src/androidTest/java/org/hisp/dhis/android/dataentry/login/LoginViewTests.java
+++ app/src/androidTest/java/org/hisp/dhis/android/dataentry/login/LoginViewTests.java
@@ -32,43 +32,36 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.hisp.dhis.android.dataentry.R;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
-@RunWith(AndroidJUnit4.class)
 @LargeTest
-public class LoginActivityEspressoTests {
-
+@RunWith(AndroidJUnit4.class)
+public class LoginViewTests {
     private static final String SERVER_URL = "http://play.dhis2.org/demo";
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
 
     @Rule
-    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
-            LoginActivity.class);
+    public ActivityTestRule<LoginActivity> loginViewRule =
+            new ActivityTestRule<>(LoginActivity.class);
+
+    private LoginRobot loginRobot;
+
+    @Before
+    public void setUp() throws Exception {
+        loginRobot = new LoginRobot();
+    }
 
     @Test
-    public void enableLoginButtonWhenFieldsAreFilled() throws Exception {
-
-        onView(withId(R.id.edittext_server_url))
-                .perform(typeText(SERVER_URL), closeSoftKeyboard());
-
-        onView(withId(R.id.edittext_username))
-                .perform(typeText(USERNAME), closeSoftKeyboard());
-
-        onView(withId(R.id.edittext_password))
-                .perform(typeText(PASSWORD), closeSoftKeyboard());
-
-        onView(withId(R.id.button_log_in)).check(matches(isEnabled()));
-
+    public void enableLoginButtonOnlyWhenAllFieldsAreFilled() {
+        loginRobot.typeServerUrl(SERVER_URL)
+                .typeUsername(USERNAME)
+                .typePassword(PASSWORD)
+                .isLoginButtonEnabled()
+                .eraseServerUrl()
+                .isLoginButtonDisabled();
     }
 }
