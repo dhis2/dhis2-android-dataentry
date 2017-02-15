d9bf5daa61fbedb7d730cb680b0f07ad5d6d71ab
luckytommy <thomas.lindsjorn@gmail.com>
2017-02-15 14:21:44 +0100

    Created a basic HomePresenter -View and -Fragment

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/main/home/HomeFragment.java
@@ -0,0 +1,33 @@
+package org.hisp.dhis.android.dataentry.main.home;
+
+import org.hisp.dhis.android.dataentry.commons.BaseFragment;
+
+import java.util.List;
+
+public class HomeFragment extends BaseFragment implements HomeView {
+
+    @Override
+    public void swapData(List<String> homeEntities) {
+
+    }
+
+    @Override
+    public void showProgressBar() {
+
+    }
+
+    @Override
+    public void hideProgressBar() {
+
+    }
+
+    @Override
+    public void showUnexpectedError(String message) {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+}
