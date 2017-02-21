005efd534456eb9139450533cc3b0823462c14af
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-21 08:34:09 +0100

    Reduced logging level to info.

--- ci.sh
+++ ci.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR=$DIR/
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-"$PROJECT_DIR"/gradlew --no-daemon --debug clean
-"$PROJECT_DIR"/gradlew --no-daemon --debug build -Dscan
-"$PROJECT_DIR"/gradlew --no-daemon --debug spoon
+"$PROJECT_DIR"/gradlew --no-daemon --info clean
+"$PROJECT_DIR"/gradlew --no-daemon --info build -Dscan
+"$PROJECT_DIR"/gradlew --no-daemon --info spoon
