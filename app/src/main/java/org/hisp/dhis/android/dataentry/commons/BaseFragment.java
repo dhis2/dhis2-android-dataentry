6f3e9c950e4dc2fcc2562fc789663dce890fe322
luckytommy <thomas.lindsjorn@gmail.com>
2017-02-24 14:41:14 +0100

    Convenience method for getting the UserComponent in a Fragment

--- app/src/main/java/org/hisp/dhis/android/dataentry/commons/BaseFragment.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/commons/BaseFragment.java
@@ -30,7 +30,9 @@ package org.hisp.dhis.android.dataentry.commons;
 
 import android.support.v4.app.Fragment;
 
+import org.hisp.dhis.android.dataentry.Components;
 import org.hisp.dhis.android.dataentry.DhisApp;
+import org.hisp.dhis.android.dataentry.user.UserComponent;
 
 public abstract class BaseFragment extends Fragment {
 
@@ -39,5 +41,9 @@ public abstract class BaseFragment extends Fragment {
         super.onDestroyView();
         ((DhisApp) getActivity().getApplicationContext())
                 .refWatcher().watch(this);
+    }
+
+    protected UserComponent getUserComponent() {
+        return ((Components) getActivity().getApplicationContext()).userComponent();
     }
 }
