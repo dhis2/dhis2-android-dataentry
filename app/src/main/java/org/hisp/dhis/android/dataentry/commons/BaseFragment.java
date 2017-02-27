479e8868977162e27aac363c7b3183d687b9479b
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-27 11:31:52 +0100

    Binding and unbinding to ButterKnife from BaseFragment.

--- app/src/main/java/org/hisp/dhis/android/dataentry/commons/BaseFragment.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/commons/BaseFragment.java
@@ -1,15 +1,34 @@
 package org.hisp.dhis.android.dataentry.commons;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import org.hisp.dhis.android.dataentry.DhisApp;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public abstract class BaseFragment extends Fragment {
+
+    @Nullable
+    private Unbinder unbinder;
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        // check if instance of fragment is leaked or not
         ((DhisApp) getActivity().getApplicationContext())
                 .refWatcher().watch(this);
+
+        // unbind butterknife
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
+
+    protected void bind(@NonNull Object target, @NonNull android.view.View view) {
+        unbinder = ButterKnife.bind(target, view);
     }
 }
