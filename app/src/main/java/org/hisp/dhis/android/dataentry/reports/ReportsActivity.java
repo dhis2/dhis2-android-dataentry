b22c86ff7160b2e55f6bbcf099b26f709e107b6e
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-27 11:29:39 +0100

    Implemented ReportsActivity as a host for the ReportsFragment.

--- app/src/main/java/org/hisp/dhis/android/dataentry/reports/ReportsActivity.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/reports/ReportsActivity.java
@@ -1,26 +1,33 @@
 package org.hisp.dhis.android.dataentry.reports;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import org.hisp.dhis.android.dataentry.R;
 
-import java.util.List;
+public class ReportsActivity extends AppCompatActivity {
+    private static final String ARG_FORM_UID = "arg:formUid";
 
-import io.reactivex.functions.Consumer;
-
-public class ReportsActivity extends AppCompatActivity implements ReportsView {
+    public static Intent create(@NonNull Activity activity, @NonNull String programUid) {
+        Intent intent = new Intent(activity, ReportsActivity.class);
+        intent.putExtra(ARG_FORM_UID, programUid);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, ReportsFragment.create(getFormUid()))
+                .commitNow();
     }
 
-    @NonNull
-    @Override
-    public Consumer<List<ReportViewModel>> renderReportViewModels() {
-        return null;
+    private String getFormUid() {
+        return getIntent().getExtras().getString(ARG_FORM_UID, "");
     }
 }
