402769fcff57b6779ae5ab9c92bd613227861624
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-02-27 11:36:32 +0100

    Simple ReportsRobot which checks toolbar title.

---
+++ app/src/androidTest/java/org/hisp/dhis/android/dataentry/reports/ReportsRobot.java
@@ -0,0 +1,22 @@
+package org.hisp.dhis.android.dataentry.reports;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static org.hamcrest.Matchers.is;
+import static org.hisp.dhis.android.dataentry.espresso.CustomViewMatchers.withToolbarTitle;
+
+final class ReportsRobot {
+    ReportsRobot() {
+        // explicit public constructor
+    }
+
+    ReportsRobot checkToolbarTitle(@NonNull String title) {
+        onView(isAssignableFrom(Toolbar.class))
+                .check(matches(withToolbarTitle(is(title))));
+        return this;
+    }
+}
