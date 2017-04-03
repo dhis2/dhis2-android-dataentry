45523e0b771f90aba23a16a9dafa429465e63ee6
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-04-03 13:38:54 +0200

    Handling onClick() events.

--- app/src/main/java/org/hisp/dhis/android/dataentry/reports/ReportsFragment.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/reports/ReportsFragment.java
@@ -21,19 +21,55 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import io.reactivex.functions.Consumer;
 
-public final class ReportsFragment extends BaseFragment implements ReportsView {
-    private static final String ARG_FORM_UID = "arg:formUid";
+public final class ReportsFragment extends BaseFragment
+        implements ReportsView, ReportsAdapter.OnReportViewModelClickListener {
+    private static final String ARG_ENTITY_UID = "arg:entityUid";
+    private static final String ARG_ENTITY_NAME = "arg:entityName";
+    private static final String ARG_ENTITY_TYPE = "arg:entityType";
 
     @BindView(R.id.recyclerview_reports)
     RecyclerView recyclerViewReports;
 
     @Inject
     ReportsPresenter presenter;
+
+    @Inject
+    ReportsNavigator reportsNavigator;
+
     ReportsAdapter reportsAdapter;
 
-    public static ReportsFragment create(@NonNull String formUid) {
+    @NonNull
+    public static ReportsFragment createForEvents(@NonNull String programUid, @NonNull String programName) {
         Bundle arguments = new Bundle();
-        arguments.putString(ARG_FORM_UID, formUid);
+        arguments.putString(ARG_ENTITY_UID, programUid);
+        arguments.putString(ARG_ENTITY_TYPE, ReportViewModel.TYPE_EVENTS);
+        arguments.putString(ARG_ENTITY_NAME, programName);
+
+        ReportsFragment reportsFragment = new ReportsFragment();
+        reportsFragment.setArguments(arguments);
+
+        return reportsFragment;
+    }
+
+    @NonNull
+    public static ReportsFragment createForTeis(@NonNull String teUid, @NonNull String teName) {
+        Bundle arguments = new Bundle();
+        arguments.putString(ARG_ENTITY_UID, teUid);
+        arguments.putString(ARG_ENTITY_TYPE, ReportViewModel.TYPE_TEIS);
+        arguments.putString(ARG_ENTITY_NAME, teName);
+
+        ReportsFragment reportsFragment = new ReportsFragment();
+        reportsFragment.setArguments(arguments);
+
+        return reportsFragment;
+    }
+
+    @NonNull
+    public static ReportsFragment createForEnrollments(@NonNull String teiUid, @NonNull String teName) {
+        Bundle arguments = new Bundle();
+        arguments.putString(ARG_ENTITY_UID, teiUid);
+        arguments.putString(ARG_ENTITY_TYPE, ReportViewModel.TYPE_ENROLLMENTS);
+        arguments.putString(ARG_ENTITY_NAME, teName);
 
         ReportsFragment reportsFragment = new ReportsFragment();
         reportsFragment.setArguments(arguments);
@@ -45,9 +81,9 @@ public final class ReportsFragment extends BaseFragment implements ReportsView {
     public void onAttach(Context context) {
         super.onAttach(context);
 
-        // inject dependencies
         ((DhisApp) context.getApplicationContext()).userComponent()
-                .plus(new ReportsModule(getFormUid()))
+                .plus(new ReportsModule(getActivity(), getEntityUid(),
+                        getEntityType(), getEntityName()))
                 .inject(this);
     }
 
@@ -86,12 +122,25 @@ public final class ReportsFragment extends BaseFragment implements ReportsView {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        reportsAdapter = new ReportsAdapter(getContext());
+        reportsAdapter = new ReportsAdapter(getContext(), this);
         recyclerViewReports.setLayoutManager(layoutManager);
         recyclerViewReports.setAdapter(reportsAdapter);
     }
 
-    private String getFormUid() {
-        return getArguments().getString(ARG_FORM_UID, "");
+    private String getEntityUid() {
+        return getArguments().getString(ARG_ENTITY_UID, "");
+    }
+
+    private String getEntityType() {
+        return getArguments().getString(ARG_ENTITY_TYPE, "");
+    }
+
+    private String getEntityName() {
+        return getArguments().getString(ARG_ENTITY_NAME, "");
+    }
+
+    @Override
+    public void onClick(@NonNull ReportViewModel reportViewModel) {
+        reportsNavigator.navigateTo(reportViewModel.id());
     }
 }
