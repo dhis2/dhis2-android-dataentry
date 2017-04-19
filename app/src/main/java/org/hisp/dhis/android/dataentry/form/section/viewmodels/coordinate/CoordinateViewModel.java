40daec5b6e2fa8efaf6e99c3ed7e60860dc8edd6
luckytommy <thomas.lindsjorn@gmail.com>
2017-04-19 15:59:19 +0200

    CoordinateViewModel, -ViewHolder and layout. This view model represents the COORDINATE valuetype

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/section/viewmodels/coordinate/CoordinateViewModel.java
@@ -0,0 +1,25 @@
+package org.hisp.dhis.android.dataentry.form.section.viewmodels.coordinate;
+
+import android.support.annotation.NonNull;
+
+import com.google.auto.value.AutoValue;
+
+import org.hisp.dhis.android.dataentry.form.section.viewmodels.EditableViewModel;
+
+@AutoValue
+public abstract class CoordinateViewModel extends EditableViewModel {
+
+    @NonNull
+    public abstract String latitude();
+
+    @NonNull
+    public abstract String longitude();
+
+    @NonNull
+    public static CoordinateViewModel create(@NonNull String uid,
+                                             @NonNull String label,
+                                             @NonNull Boolean mandatory,
+                                             @NonNull String latitude, @NonNull String longitude) {
+        return new AutoValue_CoordinateViewModel(uid, label, mandatory, latitude, longitude);
+    }
+}
