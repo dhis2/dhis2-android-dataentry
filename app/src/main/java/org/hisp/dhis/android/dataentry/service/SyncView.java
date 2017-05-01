bd4c4a98db04af431f1ddc377557115992c855fa
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-01 17:28:28 +0200

    Refactored SyncService view to consume SyncPresenter APIs.

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/service/SyncView.java
@@ -0,0 +1,13 @@
+package org.hisp.dhis.android.dataentry.service;
+
+import android.support.annotation.NonNull;
+
+import org.hisp.dhis.android.dataentry.commons.ui.View;
+
+import io.reactivex.functions.Consumer;
+
+interface SyncView extends View {
+
+    @NonNull
+    Consumer<SyncResult> render();
+}
