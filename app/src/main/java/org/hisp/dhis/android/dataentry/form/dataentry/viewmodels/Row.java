1687d55d1f10477307043ec18c24e5cc89d1c766
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-05 18:46:13 +0200

    Changed definition of the Row interface.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/Row.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/Row.java
@@ -1,18 +1,13 @@
 package org.hisp.dhis.android.dataentry.form.dataentry.viewmodels;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.ViewGroup;
 
-import org.hisp.dhis.android.dataentry.commons.tuples.Pair;
-
-import io.reactivex.observers.DisposableObserver;
-
-public interface Row<VH extends RecyclerView.ViewHolder, VM extends FormItemViewModel> {
+public interface Row {
 
     @NonNull
-    VH onCreateViewHolder(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent);
+    ViewHolder onCreate(@NonNull ViewGroup parent);
 
-    void onBindViewHolder(@NonNull VH viewHolder, @NonNull VM viewModel);
+    void onBind(@NonNull ViewHolder viewHolder, @NonNull FormItemViewModel viewModel);
 }
