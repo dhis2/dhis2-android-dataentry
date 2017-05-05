fb113679db69286d8b49c2d43e5a8983637c5682
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-05 18:27:20 +0200

    Injecting processor into CheckBoxRow. Changed the way subscription is handled for the view.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/checkbox/CheckBoxRow.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/checkbox/CheckBoxRow.java
@@ -1,34 +1,40 @@
 package org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.checkbox;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import org.hisp.dhis.android.dataentry.R;
+import org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.FormItemViewModel;
 import org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.Row;
+import org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.RowAction;
 
 import io.reactivex.processors.FlowableProcessor;
 
-public class CheckBoxRow implements Row<CheckBoxViewHolder, CheckBoxViewModel> {
+public class CheckBoxRow implements Row {
+
+    @NonNull
+    private final LayoutInflater inflater;
 
     @NonNull
     private final FlowableProcessor<RowAction> processor;
 
-    public CheckBoxRow(@NonNull FlowableProcessor<RowAction> processor) {
+    public CheckBoxRow(@NonNull LayoutInflater inflater,
+            @NonNull FlowableProcessor<RowAction> processor) {
+        this.inflater = inflater;
         this.processor = processor;
     }
 
     @NonNull
     @Override
-    public CheckBoxViewHolder onCreateViewHolder(
-            @NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {
+    public ViewHolder onCreate(@NonNull ViewGroup parent) {
         return new CheckBoxViewHolder(parent, inflater.inflate(
                 R.layout.recyclerview_row_checkbox, parent, false), processor);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CheckBoxViewHolder viewHolder,
-            @NonNull CheckBoxViewModel viewModel) {
-        viewHolder.update(viewModel);
+    public void onBind(@NonNull ViewHolder viewHolder, @NonNull FormItemViewModel viewModel) {
+        ((CheckBoxViewHolder) viewHolder).update((CheckBoxViewModel) viewModel);
     }
 }
