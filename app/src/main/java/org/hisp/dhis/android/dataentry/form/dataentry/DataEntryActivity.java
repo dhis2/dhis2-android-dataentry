e45762e4d18750c6c933cfa223b5c6da9660720f
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-05 19:14:52 +0200

    Added new activity for monkey testing purposes.

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/DataEntryActivity.java
@@ -0,0 +1,26 @@
+package org.hisp.dhis.android.dataentry.form.dataentry;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import org.hisp.dhis.android.dataentry.R;
+
+public class DataEntryActivity extends AppCompatActivity {
+    private static final String ARGS = "args";
+
+    @NonNull
+    public static Intent create(@NonNull Activity activity, @NonNull DataEntryArguments arguments) {
+        Intent intent = new Intent(activity, DataEntryActivity.class);
+        intent.putExtra(ARGS, arguments);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_entry);
+    }
+}
