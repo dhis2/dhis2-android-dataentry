e1816b4255e1c52e724017181acbf4a5a1c7eae2
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-05 18:43:07 +0200

    Refactored TextRow to fit new Row interface definition.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/text/TextRow.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/text/TextRow.java
@@ -1,24 +1,32 @@
 package org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.text;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import org.hisp.dhis.android.dataentry.R;
-import org.hisp.dhis.android.dataentry.commons.tuples.Pair;
+import org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.FormItemViewModel;
 import org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.Row;
 
-import io.reactivex.observers.DisposableObserver;
+public final class TextRow implements Row {
 
-public class TextRow implements Row<TextViewHolder, TextViewModel> {
+    @NonNull
+    private final LayoutInflater inflater;
 
+    public TextRow(@NonNull LayoutInflater inflater) {
+        this.inflater = inflater;
+    }
+
+    @NonNull
     @Override
-    public TextViewHolder onCreateViewHolder(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {
-        return new TextViewHolder(inflater.inflate(R.layout.recyclerview_row_textview, parent, false));
+    public ViewHolder onCreate(@NonNull ViewGroup parent) {
+        return new TextViewHolder(inflater.inflate(
+                R.layout.recyclerview_row_textview, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TextViewHolder viewHolder, @NonNull TextViewModel viewModel) {
-        viewHolder.update(viewModel);
+    public void onBind(@NonNull ViewHolder viewHolder, @NonNull FormItemViewModel viewModel) {
+        ((TextViewHolder) viewHolder).update((TextViewModel) viewModel);
     }
 }
--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/text/TextViewHolder.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/viewmodels/text/TextViewHolder.java
@@ -10,7 +10,7 @@ import org.hisp.dhis.android.dataentry.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class TextViewHolder extends RecyclerView.ViewHolder {
+final class TextViewHolder extends RecyclerView.ViewHolder {
 
     @BindView(R.id.textview_row_label)
     TextView label;
@@ -18,7 +18,7 @@ public class TextViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.textview_row_textview)
     TextView value;
 
-    public TextViewHolder(@NonNull View itemView) {
+    TextViewHolder(@NonNull View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
     }
