0a0cb8148f3bac9485f7879381ee737dcd3ca636
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-05 20:48:15 +0200

    SQL query implementation for the ProgramStageRepository.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/ProgramStageRepository.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/ProgramStageRepository.java
@@ -5,36 +5,62 @@ import android.support.annotation.NonNull;
 
 import com.squareup.sqlbrite.BriteDatabase;
 
+import org.hisp.dhis.android.core.event.EventModel;
 import org.hisp.dhis.android.dataentry.form.dataentry.viewmodels.FormItemViewModel;
 
 import java.util.List;
 
 import io.reactivex.Flowable;
+import timber.log.Timber;
 
 import static hu.akarnokd.rxjava.interop.RxJavaInterop.toV2Flowable;
 
 final class ProgramStageRepository implements DataEntryRepository {
+    private static final String QUERY = "SELECT\n" +
+            "  ProgramStageDataElement.compulsory,\n" +
+            "  DataElement.displayName,\n" +
+            "  DataElement.valueType,\n" +
+            "  DataElement.optionSet\n" +
+            "FROM Event\n" +
+            "  LEFT OUTER JOIN ProgramStageDataElement ON " +
+            "ProgramStageDataElement.programStage = Event.programStage\n" +
+            "  LEFT OUTER JOIN DataElement ON ProgramStageDataElement.dataElement = DataElement.uid\n" +
+            "WHERE Event.uid = ?\n" +
+            "ORDER BY ProgramStageDataElement.sortOrder";
 
     @NonNull
     private final BriteDatabase briteDatabase;
 
     @NonNull
-    private final String program;
+    private final String event;
 
-    ProgramStageRepository(@NonNull BriteDatabase briteDatabase, @NonNull String program) {
+    ProgramStageRepository(@NonNull BriteDatabase briteDatabase, @NonNull String event) {
         this.briteDatabase = briteDatabase;
-        this.program = program;
+        this.event = event;
     }
 
     @NonNull
     @Override
-    public Flowable<List<FormItemViewModel>> formItems() {
-        return toV2Flowable(briteDatabase.createQuery("", "")
+    public Flowable<List<FormItemViewModel>> fields() {
+        return toV2Flowable(briteDatabase.createQuery(EventModel.TABLE, QUERY, event)
                 .mapToList(this::transform));
     }
 
     @NonNull
     private FormItemViewModel transform(@NonNull Cursor cursor) {
-        return null;
+        Timber.d("DataElement = {%s}", cursor.getString(1));
+        return new FormItemViewModel() {
+            @NonNull
+            @Override
+            public String uid() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public String label() {
+                return null;
+            }
+        };
     }
 }
