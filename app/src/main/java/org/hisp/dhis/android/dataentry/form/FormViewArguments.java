f4331577e72d58096abeaaeaf26e1371582ac50e
luckytommy <thomas.lindsjorn@gmail.com>
2017-05-09 14:14:47 +0200

    View arguments for FormView and DataEntryView

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/FormViewArguments.java
@@ -0,0 +1,35 @@
+package org.hisp.dhis.android.dataentry.form;
+
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+
+import com.google.auto.value.AutoValue;
+
+import static org.hisp.dhis.android.dataentry.form.FormViewArguments.Type.ENROLLMENT;
+import static org.hisp.dhis.android.dataentry.form.FormViewArguments.Type.EVENT;
+
+@AutoValue
+public abstract class FormViewArguments implements Parcelable {
+
+    // this is the uid for an enrollment or an event
+    @NonNull
+    public abstract String uid();
+
+    @NonNull
+    public abstract Type type();
+
+    @NonNull
+    public static FormViewArguments createForEnrollment(@NonNull String enrollmentUid) {
+        return new AutoValue_FormViewArguments(enrollmentUid, ENROLLMENT);
+    }
+
+    @NonNull
+    public static FormViewArguments createForEvent(@NonNull String eventUid) {
+        return new AutoValue_FormViewArguments(eventUid, EVENT);
+    }
+
+    public enum Type {
+        ENROLLMENT, EVENT
+    }
+
+}
