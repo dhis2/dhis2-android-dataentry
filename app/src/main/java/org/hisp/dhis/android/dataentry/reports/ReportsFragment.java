6595e592eed4e29813086189aa2fa523c64692cf
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-09 21:58:06 +0200

    (Integration) Exposing UI events as a stream to presenter.

--- app/src/main/java/org/hisp/dhis/android/dataentry/reports/ReportsFragment.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/reports/ReportsFragment.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.jakewharton.rxbinding2.view.RxView;
 
 import org.hisp.dhis.android.dataentry.DhisApp;
 import org.hisp.dhis.android.dataentry.R;
@@ -19,6 +22,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
 
 public final class ReportsFragment extends BaseFragment
@@ -27,6 +31,9 @@ public final class ReportsFragment extends BaseFragment
 
     @BindView(R.id.recyclerview_reports)
     RecyclerView recyclerViewReports;
+
+    @BindView(R.id.fab_create)
+    FloatingActionButton buttonCreateReport;
 
     @Inject
     ReportsPresenter presenter;
@@ -92,6 +99,23 @@ public final class ReportsFragment extends BaseFragment
         return reportViewModels -> reportsAdapter.swapData(reportViewModels);
     }
 
+    @NonNull
+    @Override
+    public Observable<Object> createReportsActions() {
+        return RxView.clicks(buttonCreateReport);
+    }
+
+    @NonNull
+    @Override
+    public Consumer<String> createReport() {
+        return action -> reportsNavigator.createFor(action);
+    }
+
+    @Override
+    public void onClick(@NonNull ReportViewModel reportViewModel) {
+        reportsNavigator.navigateTo(reportViewModel.id());
+    }
+
     private void setUpRecyclerView() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -99,10 +123,5 @@ public final class ReportsFragment extends BaseFragment
         reportsAdapter = new ReportsAdapter(getContext(), this);
         recyclerViewReports.setLayoutManager(layoutManager);
         recyclerViewReports.setAdapter(reportsAdapter);
-    }
-
-    @Override
-    public void onClick(@NonNull ReportViewModel reportViewModel) {
-        reportsNavigator.navigateTo(reportViewModel.id());
     }
 }
