c26e5cba5f9450f4bf6931f93fb59a8ddfc24f25
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-09 19:45:48 +0200

    Renamed method in DataEntryRepository.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/DataEntryRepository.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/DataEntryRepository.java
@@ -11,5 +11,5 @@ import io.reactivex.Flowable;
 public interface DataEntryRepository {
 
     @NonNull
-    Flowable<List<FieldViewModel>> fields();
+    Flowable<List<FieldViewModel>> list(@NonNull String uid);
 }
