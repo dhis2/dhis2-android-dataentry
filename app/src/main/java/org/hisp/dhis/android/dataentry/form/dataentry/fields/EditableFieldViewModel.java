221224a3a911610ca87f60f3956afaa2e577b4c8
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-09 15:58:13 +0200

    Value is made @Nullable within EditableFieldViewModel.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/fields/EditableFieldViewModel.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/fields/EditableFieldViewModel.java
@@ -1,12 +1,13 @@
 package org.hisp.dhis.android.dataentry.form.dataentry.fields;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 public abstract class EditableFieldViewModel<T> extends FieldViewModel {
 
     @NonNull
     public abstract Boolean mandatory();
 
-    @NonNull
+    @Nullable
     public abstract T value();
 }
