16bdb6bd4dbfb62f21cd06236012a41559914a90
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-09 15:20:11 +0200

    Refactored RadioButtonViewModel to use enum values instead of strings.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/fields/radiobutton/RadioButtonViewModel.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/fields/radiobutton/RadioButtonViewModel.java
@@ -8,11 +8,36 @@ import com.google.auto.value.AutoValue;
 import org.hisp.dhis.android.dataentry.form.dataentry.fields.EditableFieldViewModel;
 
 @AutoValue
-public abstract class RadioButtonViewModel extends EditableFieldViewModel<Boolean> {
+public abstract class RadioButtonViewModel extends EditableFieldViewModel<RadioButtonViewModel.Value> {
+    public enum Value {
+        YES("true"), NO("false"), NONE("");
+
+        @NonNull
+        private final String value;
+
+        Value(@NonNull String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
 
     @NonNull
-    public static RadioButtonViewModel create(@NonNull String uid, @NonNull String label,
-            @NonNull Boolean mandatory, @Nullable Boolean value) {
-        return new AutoValue_RadioButtonViewModel(uid, label, mandatory, value);
+    public static RadioButtonViewModel fromRawValue(@NonNull String uid, @NonNull String label,
+            @NonNull Boolean mandatory, @Nullable String value) {
+        if (value == null) {
+            return new AutoValue_RadioButtonViewModel(uid, label, mandatory, null);
+        } else if (value.toLowerCase().equals(Value.YES.toString())) {
+            return new AutoValue_RadioButtonViewModel(uid, label, mandatory, Value.YES);
+        } else if (value.toLowerCase().equals(Value.NO.toString())) {
+            return new AutoValue_RadioButtonViewModel(uid, label, mandatory, Value.NO);
+        } else if (value.isEmpty()) {
+            return new AutoValue_RadioButtonViewModel(uid, label, mandatory, Value.NONE);
+        } else {
+            throw new IllegalArgumentException("Unsupported value: " + value);
+        }
     }
 }
