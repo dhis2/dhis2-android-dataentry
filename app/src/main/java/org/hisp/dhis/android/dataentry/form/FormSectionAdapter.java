793790d09dba2e0cbbe28dbc7fc4afbfeb506b0a
luckytommy <thomas.lindsjorn@gmail.com>
2017-05-15 16:51:18 +0200

    DataEntryViewArguments renamed to FormSectionViewModel

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/FormSectionAdapter.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/FormSectionAdapter.java
@@ -5,39 +5,38 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import org.hisp.dhis.android.dataentry.commons.ui.DummyFragment;
-import org.hisp.dhis.android.dataentry.form.dataentry.DataEntryViewArguments;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class FormSectionAdapter extends FragmentStatePagerAdapter {
 
-    private final List<DataEntryViewArguments> dataEntryViewArgumentsList;
+    private final List<FormSectionViewModel> formSectionViewModelList;
 
     FormSectionAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
-        this.dataEntryViewArgumentsList = new ArrayList<>();
+        this.formSectionViewModelList = new ArrayList<>();
     }
 
     @Override
     public Fragment getItem(int position) {
-        return DummyFragment.newInstance(dataEntryViewArgumentsList.get(position).label());
-        //return DataEntryFragment.newInstance(dataEntryViewArgumentsList.get(position));
+        return DummyFragment.newInstance(formSectionViewModelList.get(position).label());
+        //return DataEntryFragment.newInstance(formSectionViewModelList.get(position));
     }
 
     @Override
     public int getCount() {
-        return dataEntryViewArgumentsList.size();
+        return formSectionViewModelList.size();
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return dataEntryViewArgumentsList.get(position).label();
+        return formSectionViewModelList.get(position).label();
     }
 
-    void swapData(List<DataEntryViewArguments> dataEntryViewArguments) {
-        this.dataEntryViewArgumentsList.clear();
-        this.dataEntryViewArgumentsList.addAll(dataEntryViewArguments);
+    void swapData(List<FormSectionViewModel> formSectionViewArguments) {
+        this.formSectionViewModelList.clear();
+        this.formSectionViewModelList.addAll(formSectionViewArguments);
         notifyDataSetChanged();
     }
 }
