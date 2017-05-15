0b86c4c05b66d5d18e4cb1f3296c36a42ee97064
luckytommy <thomas.lindsjorn@gmail.com>
2017-05-15 16:49:32 +0200

    Store and retrieve ReportDate and Event Status in FormPresenter. The Repository emits updates to the View and the View emits updates to the Repository (two-way data binding) using rxJava streams.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/FormPresenterImpl.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/FormPresenterImpl.java
@@ -2,10 +2,14 @@ package org.hisp.dhis.android.dataentry.form;
 
 import android.support.annotation.NonNull;
 
+import org.hisp.dhis.android.core.event.EventStatus;
 import org.hisp.dhis.android.dataentry.commons.schedulers.SchedulerProvider;
+import org.hisp.dhis.android.dataentry.commons.tuples.Pair;
 import org.hisp.dhis.android.dataentry.commons.ui.View;
 
+import io.reactivex.Flowable;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Predicate;
 import timber.log.Timber;
 
 import static org.hisp.dhis.android.dataentry.commons.utils.Preconditions.isNull;
@@ -29,25 +33,76 @@ class FormPresenterImpl implements FormPresenter {
 
     @Override
     public void onAttach(@NonNull View view) {
-        isNull(view, "HomeView must not be null");
+        isNull(view, "FormView must not be null");
 
         if (view instanceof FormView) {
             FormView formView = (FormView) view;
 
-            compositeDisposable.add(formRepository.title(formView.formViewArguments().uid())
+            String reportUid = formView.formViewArguments().uid();
+
+            compositeDisposable.add(formRepository.title(reportUid)
                     .subscribeOn(schedulerProvider.io())
                     .observeOn(schedulerProvider.ui())
                     .subscribe(
                             formView.renderTitle(),
                             Timber::e));
 
-            compositeDisposable.add(formRepository.sections(formView.formViewArguments().uid())
+            compositeDisposable.add(formRepository.reportDate(reportUid)
+                    .subscribeOn(schedulerProvider.io())
+                    .observeOn(schedulerProvider.ui())
+                    .subscribe(
+                            formView.renderReportDate(),
+                            Timber::e));
+
+            compositeDisposable.add(formRepository.sections(reportUid)
                     .subscribeOn(schedulerProvider.io())
                     .observeOn(schedulerProvider.ui())
                     .subscribe(
                             formView.renderSectionViewModels(),
                             Timber::e));
+
+            compositeDisposable.add(formView.reportDateChanged()
+                    .subscribeOn(schedulerProvider.ui())
+                    .observeOn(schedulerProvider.io())
+                    .subscribe(
+                            formRepository.storeReportDate(reportUid),
+                            Timber::e));
+            
+            compositeDisposable.add(formRepository.reportStatus(reportUid)
+                    .subscribeOn(schedulerProvider.io())
+                    .distinctUntilChanged()
+                    .filter(eventStatus -> formView.formViewArguments().type() == FormViewArguments.Type.EVENT)
+                    .zipWith(Flowable.range(0, Integer.MAX_VALUE),
+                            (eventStatus, integer) -> Pair.create(integer, eventStatus))
+                    .subscribeOn(schedulerProvider.ui())
+                    .map(pair -> {
+                        if (pair.val0() != 0) {
+                            formView.renderStatusChangeSnackBar(pair.val1());
+                        }
+                        return pair.val1();
+                    })
+                    .observeOn(schedulerProvider.ui())
+                    .subscribe(formView.renderStatus(), Timber::e));
+
+            compositeDisposable.add(formView.eventStatusChanged()
+                    .filter(handleEnrollmentFabClick(formView))
+                    .subscribeOn(schedulerProvider.ui())
+                    .observeOn(schedulerProvider.io())
+                    .subscribe(
+                            formRepository.storeEventStatus(reportUid),
+                            Timber::e));
         }
+    }
+
+    @NonNull
+    private Predicate<EventStatus> handleEnrollmentFabClick(FormView formView) {
+        return eventStatus -> {
+            if (formView.formViewArguments().type() == FormViewArguments.Type.ENROLLMENT) {
+                // go to TEI dashboard screen
+                return false;
+            }
+            return true;
+        };
     }
 
     @Override
