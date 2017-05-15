e514f64186585c763a49d0744c0f6a8398824c63
luckytommy <thomas.lindsjorn@gmail.com>
2017-05-15 16:10:49 +0200

    Added functionality for storing and retrieving ReportDate and EventStatus to FormRepository.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/FormRepository.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/FormRepository.java
@@ -2,11 +2,12 @@ package org.hisp.dhis.android.dataentry.form;
 
 import android.support.annotation.NonNull;
 
-import org.hisp.dhis.android.dataentry.form.dataentry.DataEntryViewArguments;
+import org.hisp.dhis.android.core.event.EventStatus;
 
 import java.util.List;
 
 import io.reactivex.Flowable;
+import io.reactivex.functions.Consumer;
 
 public interface FormRepository {
 
@@ -17,11 +18,20 @@ public interface FormRepository {
     @NonNull
     Flowable<String> title(@NonNull String uid);
 
+    @NonNull
+    Flowable<String> reportDate(@NonNull String uid);
+
+    @NonNull
+    Flowable<EventStatus> reportStatus(@NonNull String uid);
+
     /**
      * @param uid corresponds to an event or an enrollment depending on the Repository implementation
      * @return Flowable emitting the sections to be shown in the FormView
      **/
     @NonNull
-    Flowable<List<DataEntryViewArguments>> sections(@NonNull String uid);
+    Flowable<List<FormSectionViewModel>> sections(@NonNull String uid);
 
+    Consumer<String> storeReportDate(@NonNull String uid);
+
+    Consumer<EventStatus> storeEventStatus(@NonNull String uid);
 }
