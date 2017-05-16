54bcff878f908de82d34482557ffc9afc14bf383
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-16 19:47:28 +0200

    Method for persisting data values.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/DataEntryRepository.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/DataEntryRepository.java
@@ -1,6 +1,7 @@
 package org.hisp.dhis.android.dataentry.form.dataentry;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.hisp.dhis.android.dataentry.form.dataentry.fields.FieldViewModel;
 
@@ -11,7 +12,7 @@ import io.reactivex.Flowable;
 public interface DataEntryRepository {
 
     @NonNull
-    Flowable<Long> save(@NonNull String uid, @NonNull String value);
+    Flowable<Long> save(@NonNull String uid, @Nullable String value);
 
     @NonNull
     Flowable<List<FieldViewModel>> list();
