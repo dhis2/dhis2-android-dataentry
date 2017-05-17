dfcdf0d34855f6865b5fd11d3147c97bdc942062
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-17 19:29:45 +0200

    Refactored CheckBoxViewHolder to utilize subjects for value propagation.

--- app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/fields/checkbox/CheckBoxViewHolder.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/form/dataentry/fields/checkbox/CheckBoxViewHolder.java
@@ -11,10 +11,12 @@ import com.jakewharton.rxbinding2.view.RxView;
 import com.jakewharton.rxbinding2.widget.RxCompoundButton;
 
 import org.hisp.dhis.android.dataentry.R;
+import org.hisp.dhis.android.dataentry.commons.utils.Preconditions;
 import org.hisp.dhis.android.dataentry.form.dataentry.fields.RowAction;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.processors.BehaviorProcessor;
 import io.reactivex.processors.FlowableProcessor;
 import rx.exceptions.OnErrorNotImplementedException;
 
@@ -26,26 +28,40 @@ final class CheckBoxViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.textview_row_label)
     TextView textViewLabel;
 
-    private CheckBoxViewModel viewModel;
+    @NonNull
+    BehaviorProcessor<CheckBoxViewModel> model;
 
     @SuppressWarnings("CheckReturnValue")
     CheckBoxViewHolder(@NonNull ViewGroup parent, @NonNull View itemView,
             @NonNull FlowableProcessor<RowAction> processor) {
         super(itemView);
 
+        model = BehaviorProcessor.create();
+        model.subscribe(checkBoxViewModel -> {
+            textViewLabel.setText(checkBoxViewModel.label());
+            checkBox.setChecked(CheckBoxViewModel.Value.CHECKED
+                    .equals(checkBoxViewModel.value()));
+        });
+
         ButterKnife.bind(this, itemView);
         RxCompoundButton.checkedChanges(checkBox)
-                .skipInitialValue()
                 .takeUntil(RxView.detaches(parent))
-                .map(isChecked -> RowAction.create(viewModel.uid(), String.valueOf(isChecked)))
+                .filter(isChecked -> model.hasValue())
+                .filter(isChecked -> !Preconditions.equals(
+                        model.getValue().value(), mapValue(isChecked)))
+                .map(isChecked -> RowAction.create(model.getValue().uid(),
+                        String.valueOf(mapValue(isChecked))))
                 .subscribe(action -> processor.onNext(action), throwable -> {
                     throw new OnErrorNotImplementedException(throwable);
                 });
     }
 
-    void update(@NonNull CheckBoxViewModel model) {
-        viewModel = model;
-        textViewLabel.setText(viewModel.label());
-        checkBox.setChecked(CheckBoxViewModel.Value.CHECKED.equals(viewModel.value()));
+    @NonNull
+    CheckBoxViewModel.Value mapValue(@NonNull Boolean isChecked) {
+        return isChecked ? CheckBoxViewModel.Value.CHECKED : CheckBoxViewModel.Value.UNCHECKED;
+    }
+
+    void update(@NonNull CheckBoxViewModel checkBoxViewModel) {
+        model.onNext(checkBoxViewModel);
     }
 }
