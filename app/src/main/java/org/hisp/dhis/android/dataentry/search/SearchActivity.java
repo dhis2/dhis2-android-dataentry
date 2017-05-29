6c15a9ad500ec6ce10fc430611fcd1feaf942a7b
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-05-29 14:16:25 +0200

    Added search activity. Re-implementing search.

---
+++ app/src/main/java/org/hisp/dhis/android/dataentry/search/SearchActivity.java
@@ -0,0 +1,15 @@
+package org.hisp.dhis.android.dataentry.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.hisp.dhis.android.dataentry.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
