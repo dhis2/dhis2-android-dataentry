16a75a8c7b2f6610d6f000cc59245f9f56c2f455
luckytommy <thomas.lindsjorn@gmail.com>
2017-06-01 15:51:07 +0200

    Parameterize presenters with View type

--- app/src/main/java/org/hisp/dhis/android/dataentry/commons/ui/Presenter.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/commons/ui/Presenter.java
@@ -3,9 +3,9 @@ package org.hisp.dhis.android.dataentry.commons.ui;
 import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 
-public interface Presenter {
+public interface Presenter<V extends View> {
     @UiThread
-    void onAttach(@NonNull View view);
+    void onAttach(@NonNull V view);
 
     @UiThread
     void onDetach();
