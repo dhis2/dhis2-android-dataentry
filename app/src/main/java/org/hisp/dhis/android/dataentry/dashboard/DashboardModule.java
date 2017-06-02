80adef9069e2bb59386ba937d2d915d450d0cd1b
luckytommy <thomas.lindsjorn@gmail.com>
2017-06-02 10:35:33 +0200

    Provide presenter and repository through DashboardModule

--- app/src/main/java/org/hisp/dhis/android/dataentry/dashboard/DashboardModule.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/dashboard/DashboardModule.java
@@ -2,9 +2,13 @@ package org.hisp.dhis.android.dataentry.dashboard;
 
 import android.support.annotation.NonNull;
 
+import com.squareup.sqlbrite.BriteDatabase;
+
 import org.hisp.dhis.android.dataentry.commons.dagger.PerFragment;
+import org.hisp.dhis.android.dataentry.commons.schedulers.SchedulerProvider;
 
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 @PerFragment
@@ -15,5 +19,18 @@ public class DashboardModule {
 
     public DashboardModule(@NonNull String enrollmentUid) {
         this.enrollmentUid = enrollmentUid;
+    }
+
+    @Provides
+    @PerFragment
+    DashboardPresenter dashboardPresenter(@NonNull SchedulerProvider schedulerProvider,
+                                          @NonNull DashboardRepository dashboardRepository) {
+        return new DashboardPresenterImpl(enrollmentUid, schedulerProvider, dashboardRepository);
+    }
+
+    @Provides
+    @PerFragment
+    DashboardRepository dashboardRepository(@NonNull BriteDatabase briteDatabase) {
+        return new DashboardRepositoryImpl(briteDatabase);
     }
 }
