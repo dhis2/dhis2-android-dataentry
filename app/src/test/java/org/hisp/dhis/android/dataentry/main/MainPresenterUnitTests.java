c71528198a19c882fd1395e321d1d350d317f320
luckytommy <thomas.lindsjorn@gmail.com>
2017-06-08 16:30:33 +0000

    Provide a mock representation of D2 for the MainPresenter unit tests

--- app/src/test/java/org/hisp/dhis/android/dataentry/main/MainPresenterUnitTests.java
+++ app/src/test/java/org/hisp/dhis/android/dataentry/main/MainPresenterUnitTests.java
@@ -1,5 +1,6 @@
 package org.hisp.dhis.android.dataentry.main;
 
+import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.user.UserModel;
 import org.hisp.dhis.android.dataentry.commons.schedulers.MockSchedulersProvider;
 import org.hisp.dhis.android.dataentry.user.UserRepository;
@@ -32,6 +33,9 @@ public class MainPresenterUnitTests {
     private UserRepository userRepository;
 
     @Mock
+    private D2 d2;
+
+    @Mock
     private UserModel userModel;
 
     private MainPresenter mainPresenter;
@@ -51,7 +55,7 @@ public class MainPresenterUnitTests {
         userSubject = PublishProcessor.create();
         when(userRepository.me()).thenReturn(userSubject);
 
-        mainPresenter = new MainPresenterImpl(new MockSchedulersProvider(), userRepository);
+        mainPresenter = new MainPresenterImpl(d2, new MockSchedulersProvider(), userRepository);
     }
 
     @Test
