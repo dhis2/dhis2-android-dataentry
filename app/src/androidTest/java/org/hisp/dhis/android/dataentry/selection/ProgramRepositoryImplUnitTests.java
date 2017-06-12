a80fa8dbc02e5cad9c0104e00fa2405901df1dd9
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-06-12 13:05:18 +0200

    Updated both implementation and tests of ProgramRepository.

--- app/src/androidTest/java/org/hisp/dhis/android/dataentry/selection/ProgramRepositoryImplUnitTests.java
+++ app/src/androidTest/java/org/hisp/dhis/android/dataentry/selection/ProgramRepositoryImplUnitTests.java
@@ -25,15 +25,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class ProgramRepositoryImplUnitTests {
-
-    public static final String ORGUNIT_DISPLAY_NAME = "program_set_dislayName";
-    public static final String PROGRAM_DISPLAY_NAME = "program_display_name";
-    public static final String ORGUNIT_UID = "programset_uid";
-    public static final String PROGRAM_UID = "program_uid";
-    public static final String PROGRAM_2_UID = "program2_uid";
-    public static final String PROGRAM_2_DISPLAY_NAME = "opton_2_name";
-    public static final String PROGRAM_3_UID = "program_3_uid";
-    public static final String PROGRAM_3_DISPLAY_NAME = "program_3_display_name";
+    private static final String ORGUNIT_DISPLAY_NAME = "program_set_dislayName";
+    private static final String PROGRAM_DISPLAY_NAME = "program_display_name";
+    private static final String ORGUNIT_UID = "programset_uid";
+    private static final String PROGRAM_UID = "program_uid";
+    private static final String PROGRAM_2_UID = "program2_uid";
+    private static final String PROGRAM_2_DISPLAY_NAME = "opton_2_name";
+    private static final String PROGRAM_3_UID = "program_3_uid";
+    private static final String PROGRAM_3_DISPLAY_NAME = "program_3_display_name";
 
     @Rule
     public DatabaseRule databaseRule = new DatabaseRule(Schedulers.trampoline());
@@ -55,11 +54,11 @@ public class ProgramRepositoryImplUnitTests {
         database.insert(OrganisationUnitModel.TABLE, null, orgUnit(ORGUNIT_UID, ORGUNIT_DISPLAY_NAME));
 
         database.insert(ProgramModel.TABLE, null, program(PROGRAM_UID, PROGRAM_DISPLAY_NAME));
-        database.insert(OrganisationUnitProgramLinkModel.ORGANISATION_UNIT_PROGRAM_LINK, null,
+        database.insert(OrganisationUnitProgramLinkModel.TABLE, null,
                 programOrgUnitLink(PROGRAM_UID, ORGUNIT_UID));
 
         database.insert(ProgramModel.TABLE, null, program(PROGRAM_2_UID, PROGRAM_2_DISPLAY_NAME));
-        database.insert(OrganisationUnitProgramLinkModel.ORGANISATION_UNIT_PROGRAM_LINK, null,
+        database.insert(OrganisationUnitProgramLinkModel.TABLE, null,
                 programOrgUnitLink(PROGRAM_2_UID, ORGUNIT_UID));
 
         subscriber = repository.list().test();
@@ -106,7 +105,7 @@ public class ProgramRepositoryImplUnitTests {
         subscriber.assertNotComplete();
 
         databaseRule.briteDatabase().insert(ProgramModel.TABLE, program(PROGRAM_3_UID, PROGRAM_3_DISPLAY_NAME));
-        databaseRule.briteDatabase().insert(OrganisationUnitProgramLinkModel.ORGANISATION_UNIT_PROGRAM_LINK,
+        databaseRule.briteDatabase().insert(OrganisationUnitProgramLinkModel.TABLE,
                 programOrgUnitLink(PROGRAM_3_UID, ORGUNIT_UID));
 
         subscriber.assertValueCount(3);
