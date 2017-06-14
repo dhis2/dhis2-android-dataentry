68b67a9c2dbba375881d9a1bfd0c90c0f8ae1605
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-06-14 15:33:03 +0200

    EnrollmentEventRepository implementation and intergation tests.

--- app/src/androidTest/java/org/hisp/dhis/android/dataentry/create/EnrollmentEventRepositoryIntegrationTests.java
+++ app/src/androidTest/java/org/hisp/dhis/android/dataentry/create/EnrollmentEventRepositoryIntegrationTests.java
@@ -7,9 +7,11 @@ import android.support.test.runner.AndroidJUnit4;
 import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
 import org.hisp.dhis.android.core.common.State;
 import org.hisp.dhis.android.core.enrollment.EnrollmentModel;
-import org.hisp.dhis.android.core.enrollment.EnrollmentStatus;
+import org.hisp.dhis.android.core.event.EventModel;
+import org.hisp.dhis.android.core.event.EventStatus;
 import org.hisp.dhis.android.core.organisationunit.OrganisationUnitModel;
 import org.hisp.dhis.android.core.program.ProgramModel;
+import org.hisp.dhis.android.core.program.ProgramStageModel;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceModel;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityModel;
 import org.hisp.dhis.android.dataentry.commons.utils.CodeGenerator;
@@ -29,25 +31,28 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.hisp.dhis.android.dataentry.commons.utils.CursorAssert.assertThatCursor;
 
 @RunWith(AndroidJUnit4.class)
-public class EnrollmentRepositoryIntegrationTests {
+public class EnrollmentEventRepositoryIntegrationTests {
     private static final String CURRENT_DATE = "2016-04-06T00:05:57.495";
-    private static final String[] ENROLLMENT_PROJECTION = {
-            EnrollmentModel.Columns.UID,
-            EnrollmentModel.Columns.CREATED,
-            EnrollmentModel.Columns.LAST_UPDATED,
-            EnrollmentModel.Columns.DATE_OF_ENROLLMENT,
-            EnrollmentModel.Columns.ORGANISATION_UNIT,
-            EnrollmentModel.Columns.PROGRAM,
-            EnrollmentModel.Columns.TRACKED_ENTITY_INSTANCE,
-            EnrollmentModel.Columns.ENROLLMENT_STATUS,
-            EnrollmentModel.Columns.STATE
+    private static final String[] EVENT_PROJECTION = {
+            EventModel.Columns.UID,
+            EventModel.Columns.CREATED,
+            EventModel.Columns.LAST_UPDATED,
+            EventModel.Columns.EVENT_DATE,
+            EventModel.Columns.ENROLLMENT_UID,
+            EventModel.Columns.PROGRAM,
+            EventModel.Columns.PROGRAM_STAGE,
+            EventModel.Columns.ORGANISATION_UNIT,
+            EventModel.Columns.STATUS,
+            EventModel.Columns.STATE
     };
 
-    private static final String ORGANISATION_UNIT_UID = "organisation_unit_uid";
     private static final String TRACKED_ENTITY_UID = "tracked_entity_uid";
     private static final String TEI_UID = "tei_uid";
+    private static final String ORGANISATION_UNIT_UID = "organisation_unit_uid";
     private static final String PROGRAM_UID = "program_uid";
+    private static final String PS_UID = "ps_uid";
     private static final String TEST_CODE = "test_code";
+    private static final String ENROLLMENT_UID = "enrollment_uid";
 
     @Rule
     public DatabaseRule databaseRule = new DatabaseRule(Schedulers.trampoline());
@@ -62,6 +67,15 @@ public class EnrollmentRepositoryIntegrationTests {
         orgUnit.put(OrganisationUnitModel.Columns.UID, ORGANISATION_UNIT_UID);
         db.insert(OrganisationUnitModel.TABLE, null, orgUnit);
 
+        ContentValues program = new ContentValues();
+        program.put(ProgramModel.Columns.UID, PROGRAM_UID);
+        db.insert(ProgramModel.TABLE, null, program);
+
+        ContentValues programStage = new ContentValues();
+        programStage.put(ProgramStageModel.Columns.UID, PS_UID);
+        programStage.put(ProgramStageModel.Columns.PROGRAM, PROGRAM_UID);
+        db.insert(ProgramStageModel.TABLE, null, programStage);
+
         ContentValues trackedEntity = new ContentValues();
         trackedEntity.put(TrackedEntityModel.Columns.UID, TRACKED_ENTITY_UID);
         db.insert(TrackedEntityModel.TABLE, null, trackedEntity);
@@ -72,23 +86,25 @@ public class EnrollmentRepositoryIntegrationTests {
         tei.put(TrackedEntityInstanceModel.Columns.ORGANISATION_UNIT, ORGANISATION_UNIT_UID);
         db.insert(TrackedEntityInstanceModel.TABLE, null, tei);
 
-        ContentValues program = new ContentValues();
-        program.put(ProgramModel.Columns.UID, PROGRAM_UID);
-        program.put(ProgramModel.Columns.TRACKED_ENTITY, TRACKED_ENTITY_UID);
-        db.insert(ProgramModel.TABLE, null, program);
+        ContentValues enrollment = new ContentValues();
+        enrollment.put(EnrollmentModel.Columns.UID, ENROLLMENT_UID);
+        enrollment.put(EnrollmentModel.Columns.PROGRAM, PROGRAM_UID);
+        enrollment.put(EnrollmentModel.Columns.ORGANISATION_UNIT, ORGANISATION_UNIT_UID);
+        enrollment.put(EnrollmentModel.Columns.TRACKED_ENTITY_INSTANCE, TEI_UID);
+        db.insert(EnrollmentModel.TABLE, null, enrollment);
 
         Date currentDate = BaseIdentifiableObject.DATE_FORMAT.parse(CURRENT_DATE);
 
         CodeGenerator codeGenerator = () -> TEST_CODE;
         CurrentDateProvider currentDateProvider = () -> currentDate;
-        createItemsRepository = new EnrollmentRepositoryImpl(databaseRule.briteDatabase(),
-                codeGenerator, currentDateProvider, TEI_UID);
+        createItemsRepository = new EnrollmentEventRepositoryImpl(databaseRule.briteDatabase(),
+                codeGenerator, currentDateProvider, ENROLLMENT_UID);
     }
 
     @Test
-    public void saveMustPersistEnrollmentWithCorrectProperties() {
+    public void saveMustPersistEventWithCorrectProperties() {
         TestObserver<String> testObserver = createItemsRepository.save(
-                ORGANISATION_UNIT_UID, PROGRAM_UID).test();
+                ORGANISATION_UNIT_UID, PS_UID).test();
 
         testObserver.assertNoErrors();
         testObserver.assertComplete();
@@ -97,9 +113,10 @@ public class EnrollmentRepositoryIntegrationTests {
         assertThat(testObserver.values().get(0)).isEqualTo(TEST_CODE);
 
         assertThatCursor(databaseRule.database()
-                .query(EnrollmentModel.TABLE, ENROLLMENT_PROJECTION, null, null, null, null, null))
-                .hasRow(TEST_CODE, CURRENT_DATE, CURRENT_DATE, CURRENT_DATE, ORGANISATION_UNIT_UID,
-                        PROGRAM_UID, TEI_UID, EnrollmentStatus.ACTIVE.name(), State.TO_POST.name())
+                .query(EventModel.TABLE, EVENT_PROJECTION, null, null, null, null, null))
+                .hasRow(TEST_CODE, CURRENT_DATE, CURRENT_DATE, CURRENT_DATE,
+                        ENROLLMENT_UID, PROGRAM_UID, PS_UID, ORGANISATION_UNIT_UID,
+                        EventStatus.ACTIVE.name(), State.TO_POST.name())
                 .isExhausted();
     }
 }
