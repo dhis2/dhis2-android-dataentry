53b759ed42cc75c1822019fb331499206bfd68f4
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-06-18 17:01:14 +0200

    Modified CreateItemsArguments.

--- app/src/main/java/org/hisp/dhis/android/dataentry/create/CreateItemsArgument.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/create/CreateItemsArgument.java
@@ -5,45 +5,24 @@ import android.support.annotation.NonNull;
 
 import com.google.auto.value.AutoValue;
 
-import org.hisp.dhis.android.dataentry.selection.SelectionArgument;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 @AutoValue
 public abstract class CreateItemsArgument implements Parcelable {
-
     public enum Type {
         TEI, EVENT, ENROLLMENT_EVENT, ENROLLMENT
     }
 
     @NonNull
-    public abstract String name();
+    public abstract String uid();
 
     @NonNull
-    public  abstract String uid();
+    public abstract String name();
 
     @NonNull
     public abstract Type type();
 
     @NonNull
-    public abstract List<SelectionArgument.Type> selectorTypes();
-
-    @NonNull
-    public static CreateItemsArgument create(@NonNull String name, @NonNull String uid, @NonNull Type type) {
-        SelectionArgument.Type firstType;
-        SelectionArgument.Type secondType;
-
-        if (type == Type.ENROLLMENT_EVENT) {
-            firstType = SelectionArgument.Type.PROGRAM;
-            secondType = SelectionArgument.Type.PROGRAM_STAGE;
-        } else {
-            firstType = SelectionArgument.Type.ORGANISATION;
-            secondType = SelectionArgument.Type.PROGRAM;
-        }
-        return new AutoValue_CreateItemsArgument(name, uid, type,
-                Collections.unmodifiableList(Arrays.asList(firstType, secondType)));
+    public static CreateItemsArgument create(@NonNull String uid,
+            @NonNull String name, @NonNull Type type) {
+        return new AutoValue_CreateItemsArgument(uid, name, type);
     }
-
 }
--- app/src/test/java/org/hisp/dhis/android/dataentry/create/CreateItemsArgumentUnitTests.java
+++ app/src/test/java/org/hisp/dhis/android/dataentry/create/CreateItemsArgumentUnitTests.java
@@ -1,6 +1,5 @@
 package org.hisp.dhis.android.dataentry.create;
 
-import org.hisp.dhis.android.dataentry.selection.SelectionArgument;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,63 +8,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnit4.class)
 public class CreateItemsArgumentUnitTests {
-
-    static final int FIRST = 0;
-    static final int SECOND = 1;
-
-    static final String ARG_NAME = "arg_name";
-    static final String ARG_UID = "arg_uid";
-    public static final int SIZE = 2;
+    private static final String ARG_NAME = "arg_name";
+    private static final String ARG_UID = "arg_uid";
 
     @Test
     public void createWithTEI() {
         CreateItemsArgument.Type argType = CreateItemsArgument.Type.TEI;
-        CreateItemsArgument arg = CreateItemsArgument.create(ARG_NAME, ARG_UID, argType);
+        CreateItemsArgument arg = CreateItemsArgument.create(ARG_UID, ARG_NAME, argType);
 
+        assertThat(arg.uid()).isEqualTo(ARG_UID);
         assertThat(arg.name()).isEqualTo(ARG_NAME);
-        assertThat(arg.uid()).isEqualTo(ARG_NAME);
         assertThat(arg.type()).isEqualTo(argType);
-        assertThat(arg.selectorTypes().size()).isEqualTo(SIZE);
-        assertThat(arg.selectorTypes().get(FIRST)).isEqualTo(SelectionArgument.Type.ORGANISATION);
-        assertThat(arg.selectorTypes().get(SECOND)).isEqualTo(SelectionArgument.Type.PROGRAM);
     }
 
     @Test
     public void createWithEvent() {
         CreateItemsArgument.Type argType = CreateItemsArgument.Type.EVENT;
-        CreateItemsArgument arg = CreateItemsArgument.create(ARG_NAME, ARG_UID, argType);
+        CreateItemsArgument arg = CreateItemsArgument.create(ARG_UID, ARG_NAME, argType);
 
+        assertThat(arg.uid()).isEqualTo(ARG_UID);
         assertThat(arg.name()).isEqualTo(ARG_NAME);
-        assertThat(arg.uid()).isEqualTo(ARG_NAME);
         assertThat(arg.type()).isEqualTo(argType);
-        assertThat(arg.selectorTypes().size()).isEqualTo(SIZE);
-        assertThat(arg.selectorTypes().get(FIRST)).isEqualTo(SelectionArgument.Type.ORGANISATION);
-        assertThat(arg.selectorTypes().get(SECOND)).isEqualTo(SelectionArgument.Type.PROGRAM);
     }
 
     @Test
     public void createWithEnrollment() {
         CreateItemsArgument.Type argType = CreateItemsArgument.Type.ENROLLMENT;
-        CreateItemsArgument arg = CreateItemsArgument.create(ARG_NAME, ARG_UID, argType);
+        CreateItemsArgument arg = CreateItemsArgument.create(ARG_UID, ARG_NAME, argType);
 
+        assertThat(arg.uid()).isEqualTo(ARG_UID);
         assertThat(arg.name()).isEqualTo(ARG_NAME);
-        assertThat(arg.uid()).isEqualTo(ARG_NAME);
         assertThat(arg.type()).isEqualTo(argType);
-        assertThat(arg.selectorTypes().size()).isEqualTo(SIZE);
-        assertThat(arg.selectorTypes().get(FIRST)).isEqualTo(SelectionArgument.Type.ORGANISATION);
-        assertThat(arg.selectorTypes().get(SECOND)).isEqualTo(SelectionArgument.Type.PROGRAM);
     }
 
     @Test
-    public void createWithEnrollment_Event() {
+    public void createWithEnrollmentEvent() {
         CreateItemsArgument.Type argType = CreateItemsArgument.Type.ENROLLMENT_EVENT;
-        CreateItemsArgument arg = CreateItemsArgument.create(ARG_NAME, ARG_UID, argType);
+        CreateItemsArgument arg = CreateItemsArgument.create(ARG_UID, ARG_NAME, argType);
 
+        assertThat(arg.uid()).isEqualTo(ARG_UID);
         assertThat(arg.name()).isEqualTo(ARG_NAME);
-        assertThat(arg.uid()).isEqualTo(ARG_NAME);
         assertThat(arg.type()).isEqualTo(argType);
-        assertThat(arg.selectorTypes().size()).isEqualTo(SIZE);
-        assertThat(arg.selectorTypes().get(FIRST)).isEqualTo(SelectionArgument.Type.PROGRAM);
-        assertThat(arg.selectorTypes().get(SECOND)).isEqualTo(SelectionArgument.Type.PROGRAM_STAGE);
     }
 }
