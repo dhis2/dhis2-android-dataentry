dfbba1ed4e8feb18417322ae465974799b14f5ca
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-06-18 17:01:52 +0200

    Refactored CreateItemsActivity.

--- app/src/main/java/org/hisp/dhis/android/dataentry/create/CreateItemsActivity.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/create/CreateItemsActivity.java
@@ -17,10 +17,9 @@ import butterknife.ButterKnife;
 
 import static org.hisp.dhis.android.dataentry.commons.utils.Preconditions.isNull;
 
-public class CreateItemsActivity extends AppCompatActivity {
-
-    static final String TAG_CREATE = "tag:createItems";
-    static final String ARG_CREATION = "arg:creationArguments";
+public final class CreateItemsActivity extends AppCompatActivity {
+    private static final String TAG_CREATE = "tag:createItems";
+    private static final String ARG_CREATION = "arg:creationArguments";
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -38,24 +37,11 @@ public class CreateItemsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create);
         ButterKnife.bind(this);
 
-        CreateItemsArgument argument = isNull(getIntent().getExtras().getParcelable(ARG_CREATION),
-                "CreateItems argument must be supplied.");
-
-        setupToolbar(toolbar, argument.name());
+        setupToolbar(toolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.content_frame, CreateItemsFragment.create(argument), TAG_CREATE)
+                    .add(R.id.content_frame, CreateItemsFragment.create(getArguments()), TAG_CREATE)
                     .commitNow();
-        }
-    }
-
-    private void setupToolbar(@NonNull Toolbar toolbar, @NonNull String name) {
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(name);
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
@@ -66,5 +52,21 @@ public class CreateItemsActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @NonNull
+    private CreateItemsArgument getArguments() {
+        return isNull(getIntent().getExtras().getParcelable(ARG_CREATION),
+                "CreateItemsArgument must not be null");
+    }
+
+    private void setupToolbar(@NonNull Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(getArguments().name());
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
