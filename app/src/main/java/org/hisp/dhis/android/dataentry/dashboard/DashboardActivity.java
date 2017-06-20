d4aee8d9d4d25953bbb0d0b107958c96eba628d2
luckytommy <thomas.lindsjorn@gmail.com>
2017-06-20 10:58:22 +0200

    Support two panes in DashboardActivity

--- app/src/main/java/org/hisp/dhis/android/dataentry/dashboard/DashboardActivity.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/dashboard/DashboardActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 import org.hisp.dhis.android.dataentry.R;
+import org.hisp.dhis.android.dataentry.form.FormFragment;
+import org.hisp.dhis.android.dataentry.form.FormViewArguments;
 
 import static org.hisp.dhis.android.dataentry.commons.utils.Preconditions.isNull;
 
@@ -28,13 +31,22 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form);
+        setContentView(R.layout.activity_dashboard);
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.content, DashboardFragment.newInstance(
+                .replace(R.id.navigation, DashboardFragment.newInstance(
                         getIntent().getStringExtra(ARG_ENROLLMENT_UID)))
                 .commit();
+
+        // if using two-pane layout (tablets in landscape mode), there will exist a layout for data entry
+        View dataEntry = findViewById(R.id.data_entry);
+        if (dataEntry != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.data_entry, FormFragment.newInstance(FormViewArguments.createForEvent("aygYgTAvuXy")))
+                    .commit();
+        }
 
     }
 
