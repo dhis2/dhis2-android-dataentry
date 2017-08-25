2b35bcd2ac84fbbf651fff986fe39580e9bc27ea
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-08-25 11:27:16 +0200

    Exposing RuleExpressionEvaluator within AppModule.

--- app/src/main/java/org/hisp/dhis/android/dataentry/AppModule.java
+++ app/src/main/java/org/hisp/dhis/android/dataentry/AppModule.java
@@ -3,6 +3,8 @@ package org.hisp.dhis.android.dataentry;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+import com.squareup.duktape.Duktape;
+
 import org.hisp.dhis.android.core.configuration.ConfigurationManager;
 import org.hisp.dhis.android.core.configuration.ConfigurationManagerFactory;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
@@ -10,6 +12,8 @@ import org.hisp.dhis.android.dataentry.commons.utils.CodeGenerator;
 import org.hisp.dhis.android.dataentry.commons.utils.CodeGeneratorImpl;
 import org.hisp.dhis.android.dataentry.server.ConfigurationRepository;
 import org.hisp.dhis.android.dataentry.server.ConfigurationRepositoryImpl;
+import org.hisp.dhis.rules.RuleExpressionEvaluator;
+import org.hisp.dhis.rules.android.DuktapeEvaluator;
 
 import javax.inject.Singleton;
 
@@ -29,6 +33,12 @@ final class AppModule {
     @Singleton
     Context context() {
         return application;
+    }
+
+    @Provides
+    @Singleton
+    Duktape duktape() {
+        return Duktape.create();
     }
 
     @Provides
@@ -59,5 +69,11 @@ final class AppModule {
     @Singleton
     CodeGenerator codeGenerator() {
         return new CodeGeneratorImpl();
+    }
+
+    @Provides
+    @Singleton
+    RuleExpressionEvaluator ruleExpressionEvaluator(@NonNull Duktape duktape) {
+        return new DuktapeEvaluator(duktape);
     }
 }
